53c8c7e31a4a5c8fdfba68be39f6ebdceb9272d7
GijsvdMeule <meul0090@hz.nl>
2022-09-12 11:13:52 +0200

    faq fix

--- js/faq.js
+++ js/faq.js
@@ -1,4 +1,40 @@
-function removeFunction() {
-    let p = document.getElementsByTagName('p').removeAttribute("hidden");
+function removeHidden() {
+    const p = document.querySelector('p');
+    console.log(p);
+    p.removeAttribute("hidden");
 }
+
+function removeHidden1() {
+    const x = document.querySelector(".p1");
+    console.log(x);
+    x.removeAttribute("hidden");
+}
+
+function removeHidden2() {
+    const a = document.querySelector(".p2");
+    console.log(a);
+    a.removeAttribute("hidden");
+}
+
+function removeHidden3() {
+    const r = document.querySelector(".p3");
+    console.log(r);
+    r.removeAttribute("hidden");
+}
+
+function removeHidden4() {
+    const t = document.querySelector(".p4");
+    console.log(t);
+    t.removeAttribute("hidden");
+}
+
+function removeHidden5() {
+    const y = document.querySelector(".p5");
+    console.log(y);
+    y.removeAttribute("hidden");
+}
+
+
+
+
 
